cc035a1c100c846ff15a173c085cc6120d3dc241
Rohith Gilla <gillarohith1@gmail.com>
2024-05-12 00:07:16 +0530

    maybe supabase?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/kinde", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/kinde", "@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/supabase"],
   shadcn: {
     /**
      * Prefix for all the imported component
